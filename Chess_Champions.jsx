7bb220db974adf97b88804b5f40fbff80638883d
AndreiGulii <andreigulii1985@gmail.com>
2025-07-15 17:12:43 +0200

    Cards added

--- Chess_Champions.jsx
+++ Chess_Champions.jsx
@@ -57,18 +57,39 @@ const champions = [
   },
 ];
 
+
 const Champion = (props) => {
   return (
-    <>
+    
+    <div style={{
+        border: "1px solid #ccc",
+        borderRadius: "10px",
+        padding: "10px",
+        textAlign: "center",
+        boxShadow: "0 2px 5px rgba(0,0,0,0.1)",
+        backgroundColor: "#dad7d7",
+      }}
+    >  
       <h2>Name: {props.name}</h2>
       <p>Year: {props.year}</p>
-      <img src={props.photo} alt={props.name} style={{ maxWidth: "250px" }} />
-    </>
+      <img src={props.photo} alt={props.name} style={{maxWidth: "230px", objectFit: "cover", borderRadius: "8px" }}
+      />
+    </div>
   );
 };
 
 root.render(
   <>
+  <h1 style={{textAlign: "center", backgroundColor: "grey", color: "white"}}>Чемпионы мира по шахматам</h1>
+  <div
+      className="grid-container"
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+        gap: "20px",
+        padding: "20px",
+      }}
+    >
     {champions.map((e) => (
       <Champion
         key={Math.random()}
@@ -77,5 +98,6 @@ root.render(
         photo={e.photo}
       />
     ))}
+    </div>
   </>
 );
